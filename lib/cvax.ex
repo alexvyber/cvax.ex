659b2ac27706d1ecc36da005a96b18ea46cdc6af
Alexey Sokolov <alexvyber@gmail.com>
2023-03-29 23:54:00 +0300

    cvax implemented and tested
    base - works
    variants - works
    default_variants - works
    compound_variants - TODO

--- lib/cvax.ex
+++ lib/cvax.ex
@@ -1,27 +1,26 @@
 defmodule Cvax do
+  # TODO: write docs
+  # TODO: write typespecs
+  # TODO: write functionality for compound components
+  # TODO: refactor implementaations
+
   @moduledoc """
   Documentation for `Cvax`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cvax.cn()
-      :world
-
-  """
-
-  @type class_value :: String.t() | nil | boolean() | list(class_value)
-
-  def cn() do
-    :world
-  end
-
   # cvax
   # ============================================
   @spec cx(any()) :: String.t()
+  def cx(classes) when is_binary(classes) do
+    classes
+    |> String.trim()
+    |> String.replace(~r/ +/, " ")
+  end
+
+  def cx(class: classes), do: cx(classes)
+  def cx({:class, classes}), do: cx(classes)
+  def cx(%{:class => classes}), do: cx(classes)
+
   def cx(classes) when is_list(classes) do
     List.flatten(classes)
     |> Enum.filter(fn x -> !(x == nil || x == true || x == false) end)
@@ -31,63 +30,201 @@ defmodule Cvax do
     |> String.replace(~r/ +/, " ")
   end
 
-  def cx(classes) when is_binary(classes), do: classes
   def cx({true, classes}), do: if(is_binary(classes), do: classes, else: cx(classes))
   def cx(%{true => classes}), do: if(is_binary(classes), do: classes, else: cx(classes))
   def cx(_), do: ""
 
   # variants
   # ============================================
-  @type configs :: %{
-          base: String.t(),
-          variants: %{},
-          default_variants: %{},
-          compound_variants: [%{}]
-        }
   @spec compose_variants(%{
           base: String.t(),
           variants: %{},
           default_variants: %{},
+          # TODO:
           compound_variants: [%{}]
-        }) :: (%{} | nil -> String.t())
+        }) :: (any() -> String.t())
   def compose_variants(configs) do
     case configs do
-      %{variants: variants} when is_map(variants) ->
-        fn props ->
-          if is_map(props) do
-            Enum.reduce(variants, %{defaults: [], variants: []}, fn {key, prop}, acc ->
-              case Map.get(props, key) do
-                nil ->
-                  %{
-                    defaults: [acc.defaults, prop[configs.default_variants[key]]],
-                    variants: acc.variants
-                  }
+      %{variants: variants, default_variants: default_variants}
+      when is_map(variants) and map_size(variants) > 0 and is_map(default_variants) ->
+        fn
+          props when is_map(props) and map_size(props) > 0 ->
+            get_classes_from_map(props, configs.base, variants, default_variants)
 
-                prop_key ->
-                  %{defaults: acc.defaults, variants: [acc.variants, prop[prop_key]]}
-              end
-            end)
-            |> then(fn x ->
-              cx([configs.base, x.defaults, x.variants, Map.get(props, :class)])
-            end)
-          else
-            cx([
-              configs.base,
-              Enum.map(configs.default_variants, fn {key, prop} ->
-                Map.get(configs.variants[key], prop)
-              end)
-            ])
-          end
+          props when is_list(props) ->
+            get_classes_from_list(props, configs.base, variants, default_variants)
+
+          _props ->
+            return_defaults(configs)
         end
 
-      %{base: base} when is_binary(base) ->
-        fn props -> cx([props, base, Map.get(props, :class)]) end
+      %{variants: variants}
+      when is_map(variants) and map_size(variants) > 0 ->
+        fn
+          props when is_map(props) and map_size(props) > 0 ->
+            get_classes_from_map(props, configs.base, variants)
+
+          props when is_list(props) ->
+            get_classes_from_list(props, configs.base, variants)
+
+          _props ->
+            return_defaults(configs)
+        end
+
+      %{base: base} ->
+        fn
+          %{:class => class} -> cx([base, class])
+          {:class, class} -> cx([base, class])
+          [class: class] -> cx([base, class])
+          _ -> cx(base)
+        end
 
       _ ->
-        fn props -> cx(Map.get(props, :class)) end
+        fn
+          %{:class => class} -> cx(class)
+          {:class, class} -> cx(class)
+          [class: class] -> cx(class)
+          _ -> ""
+        end
     end
   end
-end
 
-# IO.inspect(key)
-# IO.inspect(prop)
+  defp get_classes_from_list(props, base, variants) do
+    # get variants
+    Enum.reduce(
+      props,
+      %{defaults: [], variants: [], class: "", configs_variants: variants},
+      fn
+        {:class, class}, acc ->
+          Map.put(acc, :class, class)
+
+        {props_key, props_prop}, acc ->
+          case Map.has_key?(variants, props_key) do
+            true ->
+              Map.put(acc, :variants, [
+                acc.variants,
+                Map.get(variants[props_key], props_prop)
+              ])
+              |> Map.put(:configs_variants, Map.delete(acc.configs_variants, props_key))
+
+            false ->
+              acc
+          end
+      end
+    )
+
+    # get default variants
+    |> then(fn x ->
+      Enum.reduce(
+        x.configs_variants,
+        x,
+        fn _, acc ->
+          # fn {key, prop}, acc ->
+          Map.put(acc, :defaults, [acc.defaults])
+        end
+      )
+    end)
+
+    # stringify variants
+    |> then(fn
+      acc ->
+        cx([base, acc.defaults, acc.variants, acc.class])
+    end)
+  end
+
+  defp get_classes_from_list(props, base, variants, default_variants) do
+    # get variants
+    Enum.reduce(
+      props,
+      %{defaults: [], variants: [], class: "", configs_variants: variants},
+      fn
+        {:class, class}, acc ->
+          Map.put(acc, :class, class)
+
+        {props_key, props_prop}, acc ->
+          case Map.has_key?(variants, props_key) do
+            #
+            true ->
+              Map.put(acc, :variants, [
+                acc.variants,
+                Map.get(variants[props_key], props_prop)
+              ])
+              |> Map.put(:configs_variants, Map.delete(acc.configs_variants, props_key))
+
+            false ->
+              acc
+          end
+      end
+    )
+
+    # get default variants
+    |> then(fn x ->
+      Enum.reduce(
+        x.configs_variants,
+        x,
+        fn {key, prop}, acc ->
+          Map.put(acc, :defaults, [
+            acc.defaults,
+            Map.get(prop, default_variants[key])
+          ])
+        end
+      )
+    end)
+
+    # stringify variants
+    |> then(fn
+      acc ->
+        cx([base, acc.defaults, acc.variants, acc.class])
+    end)
+  end
+
+  defp get_classes_from_map(props, base, variants) do
+    Enum.reduce(variants, %{defaults: [], variants: []}, fn {key, prop}, acc ->
+      case Map.get(props, key) do
+        nil ->
+          %{
+            defaults: [
+              acc.defaults
+            ],
+            variants: acc.variants
+          }
+
+        prop_key ->
+          %{defaults: acc.defaults, variants: [acc.variants, prop[prop_key]]}
+      end
+    end)
+    |> then(fn
+      %{defaults: defaults, variants: variants} ->
+        cx([base, defaults, variants, Map.get(props, :class)])
+    end)
+  end
+
+  defp get_classes_from_map(props, base, variants, default_variants) do
+    Enum.reduce(variants, %{defaults: [], variants: []}, fn {key, prop}, acc ->
+      case Map.get(props, key) do
+        nil ->
+          %{
+            defaults: [acc.defaults, prop[Map.get(default_variants, key)]],
+            variants: acc.variants
+          }
+
+        prop_key ->
+          %{defaults: acc.defaults, variants: [acc.variants, prop[prop_key]]}
+      end
+    end)
+    |> then(fn
+      %{defaults: defaults, variants: variants} ->
+        cx([base, defaults, variants, Map.get(props, :class)])
+    end)
+  end
+
+  defp return_defaults(configs) do
+    cx([
+      configs.base,
+      Map.has_key?(configs, :default_variants) &&
+        Enum.map(configs.default_variants, fn {key, prop} ->
+          Map.get(configs.variants[key], prop)
+        end)
+    ])
+  end
+end
--- test/cvax_test.exs
+++ test/cvax_test.exs
@@ -1,14 +1,93 @@
 defmodule CvaxTest do
   use ExUnit.Case
   doctest Cvax
+  # TODO: write more detailed test descriptions
+  # TODO: write exhaustive test cases
+  # TODO: write tests for compounds variants
 
-  test "greets the world" do
-    assert Cvax.cn() == :world
+  test "empty map or any other value" do
+    configs = [nil, "string", 420, [], {}, %{}, true, false, fn -> "" end]
+
+    cases = %{
+      [%{}, "", [], {true, false}, 69, %{font_size: :sm}, nil, true, false, fn -> 1212 end] => "",
+      [
+        %{font_size: :sm, class: "shadow-sm"},
+        [class: "shadow-sm"],
+        {:class, "shadow-sm"},
+        %{class: "shadow-sm"}
+      ] => "shadow-sm"
+    }
+
+    Enum.map(configs, fn config ->
+      variants = Cvax.compose_variants(config)
+
+      Enum.map(cases, fn {inputs, output} ->
+        Enum.map(inputs, fn input -> assert variants.(input) == output end)
+      end)
+    end)
   end
 
-  describe "variants..." do
-    test "..." do
-      configs = %{
+  test "base only" do
+    configs = [
+      %{
+        base: "rounded-md"
+      },
+      %{
+        base: ["rounded-md"]
+      },
+      %{
+        base: ["rounded-md"],
+        variants: %{}
+      },
+      %{
+        base: %{class: "rounded-md"},
+        variants: nil
+      },
+      %{
+        base: [true: "rounded-md"],
+        variants: [nil]
+      },
+      %{
+        base: %{class: [true: %{class: [true: "rounded-md"]}]},
+        variants: {}
+      }
+    ]
+
+    cases = %{
+      [
+        %{},
+        "",
+        [],
+        {true, false},
+        69,
+        nil,
+        true,
+        false,
+        fn -> nil end,
+        %{class: ""}
+      ] => "rounded-md",
+      [
+        [class: "shadow-sm"],
+        [{:class, "shadow-sm"}],
+        %{class: "shadow-sm", some: "some random"}
+      ] => "rounded-md shadow-sm",
+      [
+        %{class: "shadow-sm"}
+      ] => "rounded-md shadow-sm"
+    }
+
+    Enum.map(configs, fn config ->
+      variants = Cvax.compose_variants(config)
+
+      Enum.map(cases, fn {inputs, output} ->
+        Enum.map(inputs, fn input -> assert variants.(input) == output end)
+      end)
+    end)
+  end
+
+  test "base and variants" do
+    configs = [
+      %{
         base: "rounded-md",
         variants: %{
           intent: %{
@@ -28,156 +107,233 @@ defmodule CvaxTest do
             lg: "p-4",
             xl: "p-12"
           }
-        },
-        default_variants: %{
-          size: :lg,
-          font_size: :base,
-          intent: :secondary
-        },
-        compound_Variants: %{}
+        }
       }
+    ]
 
-      # catch all
-      catch_all_variants = Cvax.compose_variants(%{})
+    cases = %{
+      [
+        %{},
+        "",
+        [],
+        {true, false},
+        69,
+        nil,
+        true,
+        false,
+        fn -> nil end,
+        %{class: ""}
+      ] => "rounded-md",
+      [
+        [class: "shadow-sm"],
+        [
+          {:class, "shadow-sm"},
+          {:some, "some"},
+          {:not_existing, "not existings"},
+          {true, "true"}
+        ],
+        %{class: "shadow-sm"}
+      ] => "rounded-md shadow-sm",
+      [
+        %{font_size: :sm, class: "shadow-sm"}
+      ] => "rounded-md text-sm shadow-sm",
+      [
+        %{font_size: :sm, size: :xl, class: "shadow-sm", intent: :primary}
+      ] => "rounded-md text-sm bg-red-300 p-12 shadow-sm",
+      [
+        [{:font_size, :sm}, {:size, :xl}, {:intent, :primary}, {:class, "shadow-sm"}]
+      ] => "rounded-md text-sm p-12 bg-red-300 shadow-sm"
+    }
 
-      assert catch_all_variants.(%{font_size: :sm, intent: :primary, class: "shadow-sm"}) ==
-               "shadow-sm"
+    Enum.map(configs, fn config ->
+      variants = Cvax.compose_variants(config)
 
-      # with only base
-      with_variants = Cvax.compose_variants(%{base: "rounded-md"})
-
-      assert with_variants.(%{font_size: :sm, intent: :primary, class: "shadow-sm"}) ==
-               "rounded-md shadow-sm"
-
-      # with variants
-      with_variants = Cvax.compose_variants(configs)
-
-      # with props
-      assert with_variants.(%{font_size: :sm, intent: :primary}) ==
-               "rounded-md p-4 text-sm bg-red-300"
-
-      # with class
-      assert with_variants.(%{font_size: :sm, intent: :primary, class: "shadow-sm"}) ==
-               "rounded-md p-4 text-sm bg-red-300 shadow-sm"
-
-      # with nil
-      assert with_variants.(nil) ==
-               "rounded-md text-base bg-slate-100 p-4"
-    end
+      Enum.map(cases, fn {inputs, output} ->
+        Enum.map(inputs, fn input -> assert variants.(input) == output end)
+      end)
+    end)
   end
 
-  describe "cx works..." do
-    test "cx works..." do
-      test_cases = %{
-        %{} => "",
-        {} => "",
-        nil => "",
-        false => "",
-        true => "",
-        fn -> "" end => "",
-        %{false: "one"} => "",
-        [%{true: "one"}, %{false: "two"}, %{true: "three"}] => "one three",
-        {true, "one"} => "one",
-        {false, "false"} => "",
-        {true,
+  test "base, variants and default variants" do
+    configs = %{
+      base: "rounded-md",
+      variants: %{
+        intent: %{
+          primary: "bg-red-300",
+          secondary: "bg-slate-100",
+          outline: "ring-1 ring-red-300"
+        },
+        font_size: %{
+          xs: "text-xs",
+          sm: "text-sm",
+          base: "text-base",
+          md: "text-md",
+          lg: "text-lg"
+        },
+        size: %{
+          sm: "p-2",
+          lg: "p-4",
+          xl: "p-12"
+        }
+      },
+      default_variants: %{
+        size: :xl,
+        font_size: :base,
+        intent: :secondary
+      }
+    }
+
+    with_variants = Cvax.compose_variants(configs)
+
+    assert with_variants.(nil) ==
+             "rounded-md text-base bg-slate-100 p-12"
+
+    assert with_variants.(%{}) ==
+             "rounded-md text-base bg-slate-100 p-12"
+
+    assert with_variants.(%{font_size: :md}) ==
+             "rounded-md bg-slate-100 p-12 text-md"
+
+    assert with_variants.(font_size: :xs) ==
+             "rounded-md bg-slate-100 p-12 text-xs"
+
+    assert with_variants.(font_size: :lg, size: :lg, intent: :outline, class: "shadow-sm") ==
+             "rounded-md text-lg p-4 ring-1 ring-red-300 shadow-sm"
+
+    assert with_variants.(%{class: "shadow-sm", size: :lg, font_size: :lg, intent: :outline}) ==
+             "rounded-md text-lg ring-1 ring-red-300 p-4 shadow-sm"
+
+    assert with_variants.(
+             size: :lg,
+             font_size: :sm,
+             class: [
+               "font-bold",
+               %{class: ["sm:text-xl", {true, "lg:text-3xl"}]}
+             ]
+           ) ==
+             "rounded-md bg-slate-100 p-4 text-sm font-bold sm:text-xl lg:text-3xl"
+
+    assert with_variants.(
+             size: :lg,
+             font_size: :sm,
+             intent: :primary,
+             not_existing: :sm,
+             class: "leading-7"
+           ) ==
+             "rounded-md p-4 text-sm bg-red-300 leading-7"
+  end
+
+  test "cx" do
+    test_cases = %{
+      %{} => "",
+      {} => "",
+      nil => "",
+      false => "",
+      true => "",
+      fn -> "" end => "",
+      %{false: "one"} => "",
+      [%{true: "one"}, %{false: "two"}, %{true: "three"}] => "one three",
+      {true, "one"} => "one",
+      {false, "false"} => "",
+      {true,
+       [
+         %{},
+         {true, "one"},
+         %{false: "false"},
+         {true, "two"},
+         {true, "three"},
+         {true, [[{true, "four"}]]},
          [
-           %{},
-           {true, "one"},
-           %{false: "false"},
-           {true, "two"},
-           {true, "three"},
-           {true, [[{true, "four"}]]},
+           "five",
            [
-             "five",
+             ["six"],
              [
-               ["six"],
+               nil,
+               false,
+               true,
+               %{},
                [
-                 nil,
-                 false,
-                 true,
-                 %{},
-                 [
-                   %{
-                     true: [
-                       {true, "seven"},
-                       %{false: "eight"},
-                       {true, "nine"},
-                       {true, [[{true, "ten"}]]}
-                     ]
-                   }
-                 ],
-                 {true, "eleven"},
-                 {false, "false"},
-                 nil
-               ]
+                 %{
+                   true: [
+                     {true, "seven"},
+                     %{false: "eight"},
+                     {true, "nine"},
+                     {true, [[{true, "ten"}]]}
+                   ]
+                 }
+               ],
+               {true, "eleven"},
+               {false, "false"},
+               nil
              ]
            ]
-         ]} => "one two three four five six seven nine ten eleven",
+         ]
+       ]} => "one two three four five six seven nine ten eleven",
+      [
+        %{true: "one"},
+        %{false: "false"},
+        %{true: "two"},
+        %{true: "three"},
+        %{true: [[%{true: "four"}]]}
+      ] => "one two three four",
+      [
+        %{},
+        %{true: "one"},
+        %{false: "false"},
+        %{true: "two"},
+        %{true: "three"},
+        %{true: [[%{true: "four"}]]},
         [
-          %{true: "one"},
-          %{false: "false"},
-          %{true: "two"},
-          %{true: "three"},
-          %{true: [[%{true: "four"}]]}
-        ] => "one two three four",
-        [
-          %{},
-          %{true: "one"},
-          %{false: "false"},
-          %{true: "two"},
-          %{true: "three"},
-          %{true: [[%{true: "four"}]]},
+          "five",
           [
-            "five",
+            ["six"],
             [
-              ["six"],
+              nil,
+              false,
+              true,
+              %{},
               [
-                nil,
-                false,
-                true,
-                %{},
-                [
-                  %{
-                    true: [
-                      %{true: "seven"},
-                      %{false: "eight"},
-                      %{true: "nine"},
-                      %{true: [[%{true: "ten"}]]}
-                    ]
-                  }
-                ],
-                {true, "eleven"},
-                {false, "false"},
-                nil
-              ]
+                %{
+                  true: [
+                    %{true: "seven"},
+                    %{false: "eight"},
+                    %{true: "nine"},
+                    %{true: [[%{true: "ten"}]]}
+                  ]
+                }
+              ],
+              {true, "eleven"},
+              {false, "false"},
+              nil
             ]
           ]
-        ] => "one two three four five six seven nine ten eleven",
-        1 => "",
+        ]
+      ] => "one two three four five six seven nine ten eleven",
+      1 => "",
 
-        # Mock classes
-        "bg-blue-500 text-white" => "bg-blue-500 text-white",
-        {true, "bg-blue-500 text-white"} => "bg-blue-500 text-white",
-        {:other, "bg-blue-500 text-white"} => "",
-        %{true: "bg-blue-500 text-white"} => "bg-blue-500 text-white",
-        %{false: "bg-blue-500 text-white"} => "",
-        %{other: "bg-blue-500 text-white"} => "",
-        [{true, "bg-blue-500 text-white"}, {false, "bg-red-500 text-black"}] =>
-          "bg-blue-500 text-white",
-        [%{true: "bg-blue-500 text-white"}, %{false: "bg-red-500 text-black"}] =>
-          "bg-blue-500 text-white",
-        [true && "bg-blue-500 text-white", true && "bg-red-500 text-black"] =>
-          "bg-blue-500 text-white bg-red-500 text-black",
-        [{true, [{true, [{true, "bg-blue-500 text-white"}]}]}] => "bg-blue-500 text-white",
-        (true && "bg-blue-500 text-white") => "bg-blue-500 text-white",
-        (false || "bg-blue-500 text-white") => "bg-blue-500 text-white",
-        # [[] | "p-4"] => "p-4",
-        "" => ""
-      }
+      # Mock classes
+      "bg-blue-500 text-white" => "bg-blue-500 text-white",
+      {true, "bg-blue-500 text-white"} => "bg-blue-500 text-white",
+      [true: "bg-blue-500 text-white"] => "bg-blue-500 text-white",
+      [other: "bg-blue-500 text-white"] => "",
+      [class: "bg-blue-500 text-white"] => "bg-blue-500 text-white",
+      {:other, "bg-blue-500 text-white"} => "",
+      %{true: "bg-blue-500 text-white"} => "bg-blue-500 text-white",
+      %{false: "bg-blue-500 text-white"} => "",
+      %{other: "bg-blue-500 text-white"} => "",
+      [{true, "bg-blue-500 text-white"}, {false, "bg-red-500 text-black"}] =>
+        "bg-blue-500 text-white",
+      [%{true: "bg-blue-500 text-white"}, %{false: "bg-red-500 text-black"}] =>
+        "bg-blue-500 text-white",
+      ["bg-blue-500 text-white", "bg-red-500 text-black"] =>
+        "bg-blue-500 text-white bg-red-500 text-black",
+      [{true, [{true, [{true, "bg-blue-500 text-white"}]}]}] => "bg-blue-500 text-white",
+      [[], "p-4"] => "p-4",
+      "" => ""
+    }
 
-      Enum.map(test_cases, fn {input, output} ->
-        assert Cvax.cx(input) == output
-      end)
-    end
+    Enum.map(test_cases, fn {input, output} ->
+      assert Cvax.cx(input) == output
+    end)
   end
 end
